f011f850d06fceafa7b2476fa9e16da9b982ef91
Jason Pepas <jasonpepas@gmail.com>
2016-02-26 22:45:14 -0600

    using a stage dir

--- unpack.sh
+++ unpack.sh
@@ -5,85 +5,95 @@ set -x
 
 source versions.bash
 
+mkdir -p stage
+
 project="cmark"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "swift-${project}-${release}" "${project}"
+    mv "swift-${project}-${release}" "${destdir}"
 fi
 
 project="llvm"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "swift-${project}-${release}" "${project}"
+    mv "swift-${project}-${release}" "${destdir}"
 fi
 
 project="lldb"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "swift-${project}-${release}" "${project}"
+    mv "swift-${project}-${release}" "${destdir}"
 fi
 
 project="llbuild"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "swift-${project}-${release}" "${project}"
+    mv "swift-${project}-${release}" "${destdir}"
 fi
 
 project="clang"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "swift-${project}-${release}" "${project}"
+    mv "swift-${project}-${release}" "${destdir}"
 fi
 
 project="swift"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "${project}-${release}" "${project}"
+    mv "${project}-${release}" "${destdir}"
 fi
 
 project="swift-corelibs-xctest"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "${project}-${release}" "${project}"
+    mv "${project}-${release}" "${destdir}"
 fi
 
 project="swift-corelibs-foundation"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "${project}" ] ; then
+destdir="stage/${project}"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "${project}-${release}" "${project}"
+    mv "${project}-${release}" "${destdir}"
 fi
 
 project="swift-package-manager"
 vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-destname="swiftpm"
-if [ ! -e "${destname}" ] ; then
+destdir="stage/swiftpm"
+if [ ! -e "${destdir}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "${project}-${release}" "${destname}"
+    mv "${project}-${release}" "${destdir}"
 fi
 
