249728e9deeff831f7e766cc59d4e95fab58a642
Jason Pepas <jasonpepas@gmail.com>
2016-02-27 13:38:11 -0600

    updating build scripts

--- build2.sh
+++ build2.sh
@@ -7,10 +7,10 @@ source versions.bash
 
 mkdir -p install products
 
-cd stage
-
 sysroot="`pwd`/sysroots/sysroot.armhf.ubuntu.trusty"
 toolchain="`pwd`/stage/gcc-arm-none-eabi-4_9-2015q3/bin"
+
+cd stage
 
 ./swift/utils/build-script -R \
     --cross-compile-targets linux-armv7 \
--- distclean.sh
+++ distclean.sh
@@ -4,3 +4,6 @@ set -eu -o pipefail
 
 ./clean.sh
 rm -rf cache products
+
+cd sysroots
+./clean.sh
