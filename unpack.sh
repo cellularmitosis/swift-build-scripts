c98da8012c9dcd3ac5e266a1470b671ee7266e90
Jason Pepas <jasonpepas@gmail.com>
2016-02-26 22:33:57 -0600

    splitting up unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -3,42 +3,87 @@
 set -eu -o pipefail
 set -x
 
-vendor="apple"
 source versions.bash
 
-for project in cmark llvm lldb llbuild clang 
-do
-    tgz="${release}.tar.gz"
-    cachedtgz="${project}.${tgz}"
-
-    if [ ! -e "${project}" ]
-    then
-        cat "cache/${cachedtgz}" | gunzip | tar x
-        mv "swift-${project}-${release}" "${project}"
-    fi
-
-done
-
-for project in swift swift-corelibs-xctest swift-corelibs-foundation
-do
-    tgz="${release}.tar.gz"
-    cachedtgz="${project}.${tgz}"
-
-    if [ ! -e "${project}" ]
-    then
-        cat "cache/${cachedtgz}" | gunzip | tar x
-        mv "${project}-${release}" "${project}"
-    fi
-
-done
-
-
-project=swift-package-manager
+project="cmark"
+vendor="apple"
 tgz="${release}.tar.gz"
 cachedtgz="${project}.${tgz}"
-if [ ! -e "swiftpm" ]
-then
+if [ ! -e "${project}" ] ; then
     cat "cache/${cachedtgz}" | gunzip | tar x
-    mv "${project}-${release}" "swiftpm"
+    mv "swift-${project}-${release}" "${project}"
+fi
+
+project="llvm"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "swift-${project}-${release}" "${project}"
+fi
+
+project="lldb"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "swift-${project}-${release}" "${project}"
+fi
+
+project="llbuild"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "swift-${project}-${release}" "${project}"
+fi
+
+project="clang"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "swift-${project}-${release}" "${project}"
+fi
+
+project="swift"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "${project}-${release}" "${project}"
+fi
+
+project="swift-corelibs-xctest"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "${project}-${release}" "${project}"
+fi
+
+project="swift-corelibs-foundation"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+if [ ! -e "${project}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "${project}-${release}" "${project}"
+fi
+
+project="swift-package-manager"
+vendor="apple"
+tgz="${release}.tar.gz"
+cachedtgz="${project}.${tgz}"
+destname="swiftpm"
+if [ ! -e "${destname}" ] ; then
+    cat "cache/${cachedtgz}" | gunzip | tar x
+    mv "${project}-${release}" "${destname}"
 fi
 
