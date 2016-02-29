46cc8986d0bc42129ea0cb4bc9f3cf089d566c1a
Jason Pepas <jasonpepas@gmail.com>
2016-02-28 18:13:05 -0600

    restructuring build scripts

---
+++ build-linux-x86_64.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+set -x
+
+source versions.bash
+
+mkdir -p install products
+
+cd stage
+
+./swift/utils/build-script \
+    --preset=buildbot_linux \
+    install_destdir="$PWD/../install" \
+    installable_package="$PWD/../products/${release}_`uname -s`_`uname -m`.tar.gz"
--- go-linux-x86_64.sh
+++ go-linux-x86_64.sh
@@ -5,7 +5,4 @@ set -eu -o pipefail
 ./clean.sh
 ./fetch.sh
 ./unpack.sh
-./patch.sh
-./build1.sh
-./build2.sh
-
+./build-linux-x86_64.sh
